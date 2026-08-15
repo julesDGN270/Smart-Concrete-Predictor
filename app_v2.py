b10bfb9b4d2e5cccd3e6a2b731ebd7c71538cde2
DEGNON <julesdegnon270@gmail.com>
2026-08-15 20:20:26 +0100

    Resolve merge conflict in app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from ui.login_window import LoginWindow
 from ui.dashboard_window import DashboardApp
 
@@ -11,10 +10,3 @@ def launch_dashboard(user):
 if __name__ == "__main__":
     login = LoginWindow(on_success=launch_dashboard)
     login.mainloop()
-=======
-from ui.dashboard_window import DashboardApp
-
-if __name__ == "__main__":
-    app = DashboardApp()
-    app.mainloop()
->>>>>>> 1280d27de547dbc305e9f55dee04900c72692f4d
